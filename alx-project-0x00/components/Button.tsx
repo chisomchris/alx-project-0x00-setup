85207d58e1c3c480edacf8e875f6a1bce1a4c811
Chisomchris <christianezeanyaeche@gmail.com>
2025-12-04 01:23:03 +0100

    added Button

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,11 @@
+import { ButtonProps } from "@/interfaces";
+
+const Button: React.FC<ButtonProps> = ({ title, styles }) => {
+  return (
+    <button className="flex justify-center  items-center bg-[#F9F9F9] rounded-full">
+      {title}
+    </button>
+  );
+};
+
+export default Button;
