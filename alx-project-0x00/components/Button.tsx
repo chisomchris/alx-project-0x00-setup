7a81755b29ac3d52a9ea594e1cca654220827121
Chisomchris <christianezeanyaeche@gmail.com>
2025-12-04 01:27:14 +0100

    added Button

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -2,7 +2,9 @@ import { ButtonProps } from "@/interfaces";
 
 const Button: React.FC<ButtonProps> = ({ title, styles }) => {
   return (
-    <button className="flex justify-center  items-center bg-[#F9F9F9] rounded-full">
+    <button
+      className={`flex justify-center  items-center bg-[#F9F9F9] ${styles}`}
+    >
       {title}
     </button>
   );
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/Button";
 import Card from "@/components/Card";
 
 const Landing: React.FC = () => {
@@ -5,6 +6,9 @@ const Landing: React.FC = () => {
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
+      <Button title="Small Rounded-sm" styles="rounded-sm" />
+      <Button title="Small Rounded-md" styles="rounded-md" />
+      <Button title="Small Rounded-full" styles="rounded-full" />
     </div>
   );
 };
